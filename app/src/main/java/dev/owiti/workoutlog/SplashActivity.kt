a92883a07c4b60d0e416e35ae08f40bb52cf151b
Owi-ti <owittiesther@gmail.com>
2022-05-21 08:53:59 +0300

    first commit

---
+++ app/src/main/java/dev/owiti/workoutlog/SplashActivity.kt
@@ -0,0 +1,11 @@
+package dev.owiti.workoutlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
