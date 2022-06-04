31e414f9e46064530420cecd31cd24282c1e6cc9
Owi-ti <owittiesther@gmail.com>
2022-06-04 08:21:43 +0300

    first commit

--- app/src/main/java/dev/owiti/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/owiti/workoutlog/LoginActivity.kt
@@ -3,7 +3,7 @@ package dev.owiti.workoutlog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
